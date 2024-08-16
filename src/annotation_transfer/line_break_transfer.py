6eeb2a1f86505dabe57f4674e2efa5ba3f2e841e
tenkalden <tenkalden007@gmail.com>
2024-08-16 11:37:02 +0530

    updated module

--- src/annotation_transfer/line_break_transfer.py
+++ src/annotation_transfer/line_break_transfer.py
@@ -2,32 +2,25 @@ from fast_antx.core import transfer
 
 
 def read_text_file(file_path):
-    try:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            return file.read()
-    except Exception as e:
-        print(f"Error reading file {file_path}: {e}")
-        return None
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return file.read()
 
 
 def write_text_file(file_path, text):
-    try:
-        with open(file_path, 'w', encoding='utf-8') as file:
-            file.write(text)
-        print(f"File saved to: {file_path}")
-    except Exception as e:
-        print(f"Error writing file {file_path}: {e}")
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(text)
+    print(f"txt saved to: {file_path}")
 
 
 def transfer_line_breaks(source_text, target_text, output_format="txt"):
-    annotation_patterns = [
+    annotations = [
         ['line_break', r'(\r?\n)']
     ]
 
     try:
         result = transfer(
             source=source_text,
-            patterns=annotation_patterns,
+            patterns=annotations,
             target=target_text,
             output=output_format
         )
@@ -42,8 +35,6 @@ def process_file(source_file_path, target_file_path, output_file_path, output_fo
     source_text = read_text_file(source_file_path)
     target_text = read_text_file(target_file_path)
 
-    if source_text is None or target_text is None:
-        return
     result = transfer_line_breaks(source_text, target_text, output_format)
     write_text_file(output_file_path, result)
 
@@ -51,7 +42,7 @@ def process_file(source_file_path, target_file_path, output_file_path, output_fo
 def main():
     source_file_path = '../../data/source_text/I768580E7.opf/base/I1PD106653.txt'
     target_file_path = '../../data/target_text/P000001.opf/base/v001.txt'
-    output_file_path = "../../data/annotated_text/line_breaks_v001.txt"
+    output_file_path = '../../data/annotated_text/line_breaks_v001.txt'
 
     process_file(source_file_path, target_file_path, output_file_path, output_format="txt")
 
